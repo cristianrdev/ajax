a96465cc15c375788fb935c0f325e2f53989028b
Your Name <crrojasserrano@gmail.com>
2021-07-20 23:08:45 -0400

    ajax added

--- apps/posts/forms/new_post.py
+++ apps/posts/forms/new_post.py
@@ -1,11 +1,15 @@
 from django.db.models.base import Model
 from apps.posts.models import Post
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['post_content']
+        widgets = {
+            'post_content': Textarea(attrs={'cols': 50, 'rows': 2, 'resize':'none' , 'class':'form-control'}),
+        }
+
         labels = {
                 'post_content': '',
                 # 'post_content': 'Contenido del Post',
